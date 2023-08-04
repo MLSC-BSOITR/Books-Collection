113e3d5a5a0e7c1060b0885d155b2c0b546a35da
Adarsh <adarshswaminath7@gmail.com>
2023-08-04 10:28:24 +0530

    new design added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { books } from "../booksCollection";
 import Card from './Components/Card';
+import Navbar from './Components/Navbar';
 
 const App = () => {
   const [searchQuery, setSearchQuery] = useState('');
@@ -21,15 +22,23 @@ const App = () => {
   });
 
   return (
-    <div className='p-4 md:p-8 lg:p-12 xl:p-16'>
+    <main>
+      <Navbar/>
+      <div className='p-4 md:p-8 lg:p-12 xl:p-16'>
       <div>
-        {/* Input field for search */}
+        {/* hero section */}
+        <div className="max-w-screen-xl flex justify-center mb-4">
+          <h2 className="text-3xl lg:text-4xl text-white font-bold font-mono">Books Collection.</h2>
+          
+        </div>
         <input
           type="text"
-          placeholder="Search by book name or author name"
+          className='mb-6 flex justify-center p-2 w-full text-gray-200 bg-[#2d2d2d] text-center rounded-full mx-auto focus:outline-none'
+          placeholder="Type by book name or author name"
           value={searchQuery}
           onChange={handleSearchInputChange}
         />
+        {/* Input field for search */}
       </div>
       <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
         {filteredBooks.map((value) => (
@@ -42,6 +51,7 @@ const App = () => {
         ))}
       </div>
     </div>
+    </main>
   );
 }
 
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Card = ({ bookName, authorName, bookLink }) => {
   return (
-    <div>
-      <div className="bg-gray-300 grid h-64">
-        <h3 className="font-bold">{bookName}</h3>
-        {authorName}
-        <a href={bookLink} target="_blank">
-          <button className="px-5 py-1.5 bg-pink-600 text-white font-bold rounded-lg">Link</button>
-        </a>
-      </div>
+    <div className="bg-[#2d2d2d] text-[#dbdbdb]  rounded-lg p-4 h-auto shadow-md w-full">
+      <h3 className="text-xl font-semibold mb-2">{bookName}</h3>
+      <p className="text-base text-[#6153ff]">{authorName}</p>
+      <a href={bookLink} target="_blank" rel="noopener noreferrer">
+        <button className="mt-4 px-5 py-1.5 text-white bg-[#2d2d2d] border font-semibold  hover:bg-[#6153ff] hover:border-none hover:rounded-full transition">
+          Get Book
+        </button>
+      </a>
     </div>
   );
 };
